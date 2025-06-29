7cc6df2d554da4af7ab68f768d9ea6ee049488d1
GIRISH <gshetecareer@gmail.com>
2025-06-29 16:27:44 +0530

    Added class level comments

--- garage-inventory-order-management-system/inventory-eureka-server/src/main/java/com/garage/eurekaserver/EurekaServerApplication.java
+++ garage-inventory-order-management-system/inventory-eureka-server/src/main/java/com/garage/eurekaserver/EurekaServerApplication.java
@@ -3,7 +3,10 @@ package com.garage.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/**
+ * EurekaServerApplication
+ *
+ * */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
